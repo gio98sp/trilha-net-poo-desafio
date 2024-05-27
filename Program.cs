a4c8f1a5e8915efd49eba81fe1942674f1b09603
Giovane Santos <giovane98sp@gmail.com>
2024-05-27 20:08:38 -0300

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("3121313123", "N1", "1234", 64);
+Iphone iphone = new Iphone("987643321", "10", "1234", 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Pou");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
